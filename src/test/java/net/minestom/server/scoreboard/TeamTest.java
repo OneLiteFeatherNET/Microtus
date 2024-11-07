5ec477332ba11eacd3c7a78a77d29e99d6eed077
theEvilReaper <steffenwx@gmail.com>
2024-11-07 10:19:41 +0100

    Add tests for the flag usage

--- src/test/java/net/minestom/server/scoreboard/TeamTest.java
+++ src/test/java/net/minestom/server/scoreboard/TeamTest.java
@@ -1,5 +1,6 @@
 package net.minestom.server.scoreboard;
 
+import it.unimi.dsi.fastutil.bytes.ByteConsumer;
 import net.kyori.adventure.text.Component;
 import net.minestom.server.entity.Player;
 import net.minestom.server.instance.Instance;
@@ -107,5 +108,105 @@ class TeamTest {
         Team givenTeam = teamManager.getTeam(TEST_NAME);
         assertNotNull(givenTeam);
         assertEquals(team, givenTeam);
+    }
+
+    @Test
+    void testFriendlyFireFlag(@NotNull Env env) {
+        Team team = Team.builder(TEST_NAME).build();
+
+        assertNotNull(team);
+        assertFalse(team.allowFriendlyFire());
+        assertFalse(team.canSeeInvisiblePlayers());
+
+        TeamsPacket packet = team.createTeamsCreationPacket();
+        assertTeamCreationFlag(packet, (byte) 0, b -> assertFalse(unpackFriendlyFire(b)));
+
+        team.updateFriendlyFireFlag();
+
+        TeamsPacket updatedTeamPacket = team.createTeamsCreationPacket();
+        assertTeamCreationFlag(updatedTeamPacket, (byte) 1, b -> assertTrue(unpackFriendlyFire(b)));
+    }
+
+    @Test
+    void testInvisibleFlag(@NotNull Env env) {
+        Team team = Team.builder(TEST_NAME).build();
+
+        assertNotNull(team);
+        assertFalse(team.allowFriendlyFire());
+        assertFalse(team.canSeeInvisiblePlayers());
+
+        TeamsPacket packet = team.createTeamsCreationPacket();
+        assertTeamCreationFlag(packet, (byte) 0, b -> assertFalse(unpackSeeInvisible(b)));
+
+        team.updateSeeInvisiblePlayersFlag();
+
+        TeamsPacket updatedTeamPacket = team.createTeamsCreationPacket();
+        assertTeamCreationFlag(updatedTeamPacket, (byte) 2, b -> assertTrue(unpackSeeInvisible(b)));
+    }
+
+    @Test
+    void testFlagRemove(@NotNull Env env) {
+        Team team = Team.builder(TEST_NAME).allowFriendlyFire().seeInvisiblePlayers().build();
+
+        assertNotNull(team);
+        assertTrue(team.allowFriendlyFire());
+        assertTrue(team.canSeeInvisiblePlayers());
+
+        TeamsPacket packet = team.createTeamsCreationPacket();
+        assertTeamCreationFlag(packet, (byte) 3, id -> {
+            assertTrue(unpackFriendlyFire(id));
+            assertTrue(unpackSeeInvisible(id));
+        });
+
+        team.updateFriendlyFireFlag();
+
+        TeamsPacket updatedTeamPacket = team.createTeamsCreationPacket();
+        assertTeamCreationFlag(updatedTeamPacket, (byte) 2, id -> {
+            assertFalse(unpackFriendlyFire(id));
+            assertTrue(unpackSeeInvisible(id));
+        });
+
+        team.updateSeeInvisiblePlayersFlag();
+
+        TeamsPacket removedTeamPacket = team.createTeamsCreationPacket();
+        assertTeamCreationFlag(removedTeamPacket, (byte) 0, id -> {
+            assertFalse(unpackFriendlyFire(id));
+            assertFalse(unpackSeeInvisible(id));
+        });
+    }
+
+    /**
+     * Asserts that the given {@link TeamsPacket} is a {@link TeamsPacket.CreateTeamAction} and that the flag is set correctly.
+     *
+     * @param teamsPacket the packet to check
+     * @param flag        the expected flag
+     * @param flagChecker the flag checker
+     */
+    private void assertTeamCreationFlag(@NotNull TeamsPacket teamsPacket, byte flag, @NotNull ByteConsumer flagChecker) {
+        assertNotNull(teamsPacket);
+        assertInstanceOf(TeamsPacket.CreateTeamAction.class, teamsPacket.action());
+
+        TeamsPacket.CreateTeamAction givenAction = (TeamsPacket.CreateTeamAction) teamsPacket.action();
+
+        assertEquals(flag, givenAction.friendlyFlags());
+        flagChecker.accept(givenAction.friendlyFlags());
+    }
+
+    /**
+     * Unpacks the friendly fire flag from the given byte.
+     * @param flags the flags to unpack
+     * @return {@code true} if the friendly fire flag is set
+     */
+    private boolean unpackFriendlyFire(int flags) {
+        return (flags & Team.ALLOW_FRIENDLY_FIRE_BIT) > 0;
+    }
+
+    /**
+     * Unpacks the see invisible flag from the given byte.
+     * @param flags the flags to unpack
+     * @return {@code true} if the see invisible flag is set
+     */
+    private boolean unpackSeeInvisible(int flags) {
+        return (flags & Team.SEE_INVISIBLE_PLAYERS_BIT) > 0;
     }
 }
