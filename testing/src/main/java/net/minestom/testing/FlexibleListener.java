3e96672d9e26535507e7440e2b615873fd76d3ea
theEvilReaper <steffenwx@gmail.com>
2024-11-11 13:08:31 +0100

    Add documentation

--- testing/src/main/java/net/minestom/testing/FlexibleListener.java
+++ testing/src/main/java/net/minestom/testing/FlexibleListener.java
@@ -5,12 +5,23 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Consumer;
 
+/**
+ * A FlexibleListener is a listener construct which works different from the normal listener code from Minestom.
+ * The use case is to follow listener during a test and can be used that the event was called or not
+ *
+ * @param <E> the event type
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public interface FlexibleListener<E extends Event> {
     /**
      * Updates the handler. Fails if the previous followup has not been called.
      */
     void followup(@NotNull Consumer<E> handler);
 
+    /**
+     * Default followup which does nothing.
+     */
     default void followup() {
         followup(event -> {
             // Empty
