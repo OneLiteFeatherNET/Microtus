1293839e5c57366c3d1ab95d3f5055d3efad65a6
OneLiteFeather <seelenretterin@onelitefeather.net>
2024-11-13 19:22:41 +0100

    Replace assert in DataComponent with Nullpointerexception

--- src/main/java/net/minestom/server/component/DataComponentMapImpl.java
+++ src/main/java/net/minestom/server/component/DataComponentMapImpl.java
@@ -150,7 +150,9 @@ record DataComponentMapImpl(@NotNull Int2ObjectMap<Object> components) implement
                     buffer.write(NetworkBuffer.VAR_INT, entry.getIntKey());
                     //noinspection unchecked
                     DataComponent<Object> type = (DataComponent<Object>) this.idToType.apply(entry.getIntKey());
-                    assert type != null;
+                    if (type == null) {
+                        throw new NullPointerException("DataComponent type should not be null"); //TODO: Check/Find out if this is right
+                    }
                     type.write(buffer, entry.getValue());
                 }
             }
