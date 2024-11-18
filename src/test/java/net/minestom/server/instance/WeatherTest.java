3175ae3e94ef3412420f724577465e842c332aac
theEvilReaper <steffenwx@gmail.com>
2024-11-18 10:54:35 +0100

    Refactor weather test and use connect without a position parameter

--- src/test/java/net/minestom/server/instance/WeatherTest.java
+++ src/test/java/net/minestom/server/instance/WeatherTest.java
@@ -1,9 +1,11 @@
 package net.minestom.server.instance;
 
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
 import net.minestom.server.network.packet.server.play.ChangeGameStatePacket;
 import net.minestom.testing.Env;
 import net.minestom.testing.extension.MicrotusExtension;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -14,11 +16,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @ExtendWith(MicrotusExtension.class)
 class WeatherTest {
-    @Test
-    void weatherTest(Env env) {
-        var instance = env.createFlatInstance();
 
-        // Defaults
+    @Test
+    void testWeatherDefaults(@NotNull Env env) {
+        Instance instance = env.createFlatInstance();
         Weather weather = instance.getWeather();
         assertFalse(weather.isRaining());
         assertEquals(0, weather.rainLevel());
@@ -27,20 +28,29 @@ class WeatherTest {
         instance.setWeather(new Weather(1, 0.5f), 1);
         instance.tick(0);
 
+        env.destroyInstance(instance);
+    }
+
+    @Test
+    void testWeatherSendOnJoin(@NotNull Env env) {
+        Instance instance = env.createFlatInstance();
+
         // Weather sent on instance join
         var connection = env.createConnection();
-        var tracker = connection.trackIncoming(ChangeGameStatePacket.class);
-        connection.connect(instance, new Pos(0, 0, 0)).join();
+        var tracker = connection.trackIncoming(ChangeGameStatePacket.class)
+                ;
+        Player player = connection.connect(instance).join();
+        assertEquals(Pos.ZERO, player.getPosition());
         tracker.assertCount(4);
         List<ChangeGameStatePacket> packets = tracker.collect();
-        var state = packets.get(0);
+        var state = packets.getFirst();
         assertEquals(ChangeGameStatePacket.Reason.BEGIN_RAINING, state.reason());
 
         state = packets.get(1);
         assertEquals(ChangeGameStatePacket.Reason.RAIN_LEVEL_CHANGE, state.reason());
         assertEquals(1, state.value());
 
-        state = packets.get(2);
+        state = packets.getLast();
         assertEquals(ChangeGameStatePacket.Reason.THUNDER_LEVEL_CHANGE, state.reason());
         assertEquals(0.5f, state.value());
 
@@ -48,7 +58,7 @@ class WeatherTest {
         var tracker2 = connection.trackIncoming(ChangeGameStatePacket.class);
         instance.setWeather(new Weather(0, 0), 2);
         instance.tick(0);
-        state = tracker2.collect().get(0);
+        state = tracker2.collect().getFirst();
         assertEquals(ChangeGameStatePacket.Reason.RAIN_LEVEL_CHANGE, state.reason());
         assertEquals(0.5f, state.value());
     }
