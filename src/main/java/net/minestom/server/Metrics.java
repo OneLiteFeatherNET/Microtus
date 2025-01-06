Minestom/Minestom

Apply microtus patches 1-20

src/main/java/net/minestom/server/Metrics.java
@@ -1,0 +1,147 @@
+package net.minestom.server;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import net.minestom.server.extras.MojangAuth;
+import net.minestom.server.extras.bungee.BungeeCordProxy;
+import net.minestom.server.extras.velocity.VelocityProxy;
+import net.minestom.server.utils.debug.DebugUtils;
+import org.bstats.MetricsBase;
+import org.bstats.charts.DrilldownPie;
+import org.bstats.charts.SimplePie;
+import org.bstats.charts.SingleLineChart;
+import org.bstats.json.JsonObjectBuilder;
+
+public class Metrics {
+    private static String bStatsUuid = System.getProperty("minestom.bstats.id");
+    private final static int SERVICE_ID = 20684;
+    private MetricsBase metrics;
+
+    public void start() {
+        MinecraftServer.LOGGER.info("Enable bstats.");
+        String serverUUID = bStatsUuid;
+        if (serverUUID == null) {
+            Path bStatsFile = Path.of(".bstats");
+            if (Files.notExists(bStatsFile)) {
+                try {
+                    Files.createFile(bStatsFile);
+                } catch (IOException e) {
+                    MinecraftServer.LOGGER.error("BStats file cannot created.");
+                    return;
+                }
+                try {
+                    Files.writeString(bStatsFile, UUID.randomUUID().toString());
+                } catch (IOException e) {
+                    MinecraftServer.LOGGER.error("BStats file cannot be written.");
+                    return;
+                }
+            }
+            try {
+                serverUUID = Files.readString(bStatsFile);
+            } catch (IOException e) {
+                MinecraftServer.LOGGER.error("BStats file cannot be readed.");
+                return;
+            }
+        }
+        System.setProperty("bstats.relocatecheck", "false");
+        metrics = new MetricsBase("server-implementation", serverUUID, SERVICE_ID,true,  this::getServerData, jsonObjectBuilder -> {}, null, () -> true, MinecraftServer.LOGGER::error,MinecraftServer.LOGGER::info,
+                DebugUtils.INSIDE_TEST, DebugUtils.INSIDE_TEST,DebugUtils.INSIDE_TEST);
+
+        metrics.addCustomChart(new SimplePie("minecraft_version", () -> {
+            String minecraftVersion = MinecraftServer.VERSION_NAME;
+            minecraftVersion = minecraftVersion.substring(minecraftVersion.indexOf("MC: ") + 4, minecraftVersion.length() - 1);
+            return minecraftVersion;
+        }));
+
+        metrics.addCustomChart(new SingleLineChart("players", () -> MinecraftServer.getConnectionManager().getOnlinePlayers().size()));
+        metrics.addCustomChart(new SimplePie("online_mode", () -> {
+            if (MojangAuth.isEnabled()) {
+                return "online";
+            } else if (VelocityProxy.isEnabled() || BungeeCordProxy.isEnabled()) {
+                return "proxied";
+            } else {
+                return "offline";
+            }
+        }));
+        final String paperVersion = "git-Microtus-%s-%s".formatted(Git.branch(), Git.commit());
+        metrics.addCustomChart(new SimplePie("minestom_version", () -> paperVersion));
+        metrics.addCustomChart(new DrilldownPie("java_version", () -> {
+            Map<String, Map<String, Integer>> map = new HashMap<>();
+            String javaVersion = System.getProperty("java.version");
+            Map<String, Integer> entry = new HashMap<>();
+            entry.put(javaVersion, 1);
+
+            // http://openjdk.java.net/jeps/223
+            // Java decided to change their versioning scheme and in doing so modified the java.version system
+            // property to return $major[.$minor][.$secuity][-ea], as opposed to 1.$major.0_$identifier
+            // we can handle pre-9 by checking if the "major" is equal to "1", otherwise, 9+
+            String majorVersion = javaVersion.split("\\.")[0];
+            String release;
+
+            int indexOf = javaVersion.lastIndexOf('.');
+
+            if (majorVersion.equals("1")) {
+                release = "Java " + javaVersion.substring(0, indexOf);
+            } else {
+                // of course, it really wouldn't be all that simple if they didn't add a quirk, now would it
+                // valid strings for the major may potentially include values such as -ea to deannotate a pre release
+                Matcher versionMatcher = Pattern.compile("\\d+").matcher(majorVersion);
+                if (versionMatcher.find()) {
+                    majorVersion = versionMatcher.group(0);
+                }
+                release = "Java " + majorVersion;
+            }
+            map.put(release, entry);
+
+            return map;
+        }));
+
+        metrics.addCustomChart(new DrilldownPie("extensions", () -> {
+            Map<String, Map<String, Integer>> map = new HashMap<>();
+
+            // count legacy plugins
+            int extensions = MinecraftServer.getExtensionManager().getExtensions().size();
+
+            // insert real value as lower dimension
+            Map<String, Integer> entry = new HashMap<>();
+            entry.put(String.valueOf(extensions), 1);
+
+            // create buckets as higher dimension
+            if (extensions == 0) {
+                map.put("0 \uD83D\uDE0E", entry); // :sunglasses:
+            } else if (extensions <= 5) {
+                map.put("1-5", entry);
+            } else if (extensions <= 10) {
+                map.put("6-10", entry);
+            } else if (extensions <= 25) {
+                map.put("11-25", entry);
+            } else if (extensions <= 50) {
+                map.put("26-50", entry);
+            } else {
+                map.put("50+ \uD83D\uDE2D", entry); // :cry:
+            }
+
+            return map;
+        }));
+    }
+
+    public void shutdown() {
+        if (this.metrics != null) {
+            this.metrics.shutdown();
+        }
+    }
+
+    private void getServerData(JsonObjectBuilder builder) {
+        builder.appendField("osName", System.getProperty("os.name"));
+        builder.appendField("osArch", System.getProperty("os.arch"));
+        builder.appendField("osVersion", System.getProperty("os.version"));
+        builder.appendField("coreCount", Runtime.getRuntime().availableProcessors());
+    }
+
+}

OneLiteFeatherNET/Microtus

(cherry picked from commit ab0874cf0515963d1f4037d3e606db241f03fadb)
Fix build grade
Add missing adventure nbt lib
Add missing dependencies
Remove old biome, banner, shield, particle, attribute system
Fix forward data generator
Disable stress tester for compile reasons
Remove old network stack
Remove no longer exists api form player
Fix forward bstats
Fix forward server flags
Fix forward terminal color converter
Fix forward inventory tests
Fix forward frog meta test
Update files like in microtus main
(cherry picked from commit d3ad5aff721abd014bf338de544a64f8a653d0cd)
Apply better workflow
(cherry picked from commit ccf63cb7de796596807e45ebb458184d4fbed4ae)
Move miss landed classes after patching source
(cherry picked from commit 2f2dec233f123c1433fb2b8e28b84eefe34381b6)
Fix workflows for java 21
(cherry picked from commit b501f93097161841515695f11bf9ac29e3896c2d)
(cherry picked from commit f35341a08bd4c0354cf9f40cc3243ebf74a0b76f)
[Chore] Update gradle to 8.7
(cherry picked from commit cf0f73caff61bb7843548b3e77a5bab4bfe905aa)
[Chore] Remove preview feature
(cherry picked from commit ab6a351f9360b5458c7e0bf5b0c8c9b9fb9452cf)
[Bugfix] Improve version string for bstats
(cherry picked from commit d63c883f64d02c133a3ea873b5a099526c0cfdc6)
Update README.md
(cherry picked from commit 9fd02fd9de3fac3ae4902c2e01fcfac12aa9d563)
Update snapshot and release version
(cherry picked from commit 18cfbade4d98b94596ead39f696ba34b0fe4fe5a)
[Chore] Replace e.printstacktrace with exception manager call or logger
(cherry picked from commit c61f7c57ada380372c1c02ffc74c2928675fe8a9)
Create CNAME
(cherry picked from commit 0a8f6dd081530583ffde47b411429351663440ae)
Bump snapshot version (#31)
(cherry picked from commit 0c32b88086895d41e0cdc053bb1b5ab9d47f84a4)
Update build.gradle.kts
(cherry picked from commit 4023e8ae4cd36057b6fb08dbb3d58616e730fed2)
Improve license usage
(cherry picked from commit e7298d4157ce88eaa0b2adf041694573e24f6f5e)
(cherry picked from commit 9574b21d38f5e81cd2b875

src/main/java/net/minestom/server/Metrics.java
@@ -1,0 +1,146 @@
+package net.minestom.server;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import net.minestom.server.extras.MojangAuth;
+import net.minestom.server.extras.bungee.BungeeCordProxy;
+import net.minestom.server.extras.velocity.VelocityProxy;
+import org.bstats.MetricsBase;
+import org.bstats.charts.DrilldownPie;
+import org.bstats.charts.SimplePie;
+import org.bstats.charts.SingleLineChart;
+import org.bstats.json.JsonObjectBuilder;
+
+public class Metrics {
+    private static String bStatsUuid = System.getProperty("minestom.bstats.id");
+    private final static int SERVICE_ID = 20684;
+    private MetricsBase metrics;
+
+    public void start() {
+        MinecraftServer.LOGGER.info("Enable bstats.");
+        String serverUUID = bStatsUuid;
+        if (serverUUID == null) {
+            Path bStatsFile = Path.of(".bstats");
+            if (Files.notExists(bStatsFile)) {
+                try {
+                    Files.createFile(bStatsFile);
+                } catch (IOException e) {
+                    MinecraftServer.LOGGER.error("BStats file cannot created.");
+                    return;
+                }
+                try {
+                    Files.writeString(bStatsFile, UUID.randomUUID().toString());
+                } catch (IOException e) {
+                    MinecraftServer.LOGGER.error("BStats file cannot be written.");
+                    return;
+                }
+            }
+            try {
+                serverUUID = Files.readString(bStatsFile);
+            } catch (IOException e) {
+                MinecraftServer.LOGGER.error("BStats file cannot be readed.");
+                return;
+            }
+        }
+        System.setProperty("bstats.relocatecheck", "false");
+        metrics = new MetricsBase("server-implementation", serverUUID, SERVICE_ID,true,  this::getServerData, jsonObjectBuilder -> {}, null, () -> true, MinecraftServer.LOGGER::error,MinecraftServer.LOGGER::info,
+                ServerFlag.INSIDE_TEST, ServerFlag.INSIDE_TEST,ServerFlag.INSIDE_TEST);
+
+        metrics.addCustomChart(new SimplePie("minecraft_version", () -> {
+            String minecraftVersion = MinecraftServer.VERSION_NAME;
+            minecraftVersion = minecraftVersion.substring(minecraftVersion.indexOf("MC: ") + 4, minecraftVersion.length() - 1);
+            return minecraftVersion;
+        }));
+
+        metrics.addCustomChart(new SingleLineChart("players", () -> MinecraftServer.getConnectionManager().getOnlinePlayers().size()));
+        metrics.addCustomChart(new SimplePie("online_mode", () -> {
+            if (MojangAuth.isEnabled()) {
+                return "online";
+            } else if (VelocityProxy.isEnabled() || BungeeCordProxy.isEnabled()) {
+                return "proxied";
+            } else {
+                return "offline";
+            }
+        }));
+        final String version = "%s-%s-@%s".formatted(MinecraftServer.VERSION_NAME,Git.branch(), Git.commit());
+        metrics.addCustomChart(new SimplePie("minestom_version", () -> version));
+        metrics.addCustomChart(new DrilldownPie("java_version", () -> {
+            Map<String, Map<String, Integer>> map = new HashMap<>();
+            String javaVersion = System.getProperty("java.version");
+            Map<String, Integer> entry = new HashMap<>();
+            entry.put(javaVersion, 1);
+
+            // http://openjdk.java.net/jeps/223
+            // Java decided to change their versioning scheme and in doing so modified the java.version system
+            // property to return $major[.$minor][.$secuity][-ea], as opposed to 1.$major.0_$identifier
+            // we can handle pre-9 by checking if the "major" is equal to "1", otherwise, 9+
+            String majorVersion = javaVersion.split("\\.")[0];
+            String release;
+
+            int indexOf = javaVersion.lastIndexOf('.');
+
+            if (majorVersion.equals("1")) {
+                release = "Java " + javaVersion.substring(0, indexOf);
+            } else {
+                // of course, it really wouldn't be all that simple if they didn't add a quirk, now would it
+                // valid strings for the major may potentially include values such as -ea to deannotate a pre release
+                Matcher versionMatcher = Pattern.compile("\\d+").matcher(majorVersion);
+                if (versionMatcher.find()) {
+                    majorVersion = versionMatcher.group(0);
+                }
+                release = "Java " + majorVersion;
+            }
+            map.put(release, entry);
+
+            return map;
+        }));
+
+        metrics.addCustomChart(new DrilldownPie("extensions", () -> {
+            Map<String, Map<String, Integer>> map = new HashMap<>();
+
+            // count legacy plugins
+            int extensions = MinecraftServer.getExtensionManager().getExtensions().size();
+
+            // insert real value as lower dimension
+            Map<String, Integer> entry = new HashMap<>();
+            entry.put(String.valueOf(extensions), 1);
+
+            // create buckets as higher dimension
+            if (extensions == 0) {
+                map.put("0 \uD83D\uDE0E", entry); // :sunglasses:
+            } else if (extensions <= 5) {
+                map.put("1-5", entry);
+            } else if (extensions <= 10) {
+                map.put("6-10", entry);
+            } else if (extensions <= 25) {
+                map.put("11-25", entry);
+            } else if (extensions <= 50) {
+                map.put("26-50", entry);
+            } else {
+                map.put("50+ \uD83D\uDE2D", entry); // :cry:
+            }
+
+            return map;
+        }));
+    }
+
+    public void shutdown() {
+        if (this.metrics != null) {
+            this.metrics.shutdown();
+        }
+    }
+
+    private void getServerData(JsonObjectBuilder builder) {
+        builder.appendField("osName", System.getProperty("os.name"));
+        builder.appendField("osArch", System.getProperty("os.arch"));
+        builder.appendField("osVersion", System.getProperty("os.version"));
+        builder.appendField("coreCount", Runtime.getRuntime().availableProcessors());
+    }
+
+}